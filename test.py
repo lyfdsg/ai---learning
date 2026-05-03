ebae43c8b618d8eb7c5594a423b95087659d7517
lyfdsg <2978024276@qq.com>
2026-05-03 16:46:09 +0800

    第一次提交：添加test.ipynb和test.py文件

---
+++ test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+print("✅ numpy 版本:", np.__version__)
+print("✅ pandas 版本:", pd.__version__)
+print("✅ matplotlib 版本:", plt.__version__)
+print("所有库导入成功！")
